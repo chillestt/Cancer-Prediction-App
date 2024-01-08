73da0c97335917b45ef21dcddbd310f89e7f8f84
Chí <minhchi1804@gmail.com>
2024-01-08 19:07:15 +0700

    update model

--- app/main.py
+++ app/main.py
@@ -53,8 +53,8 @@ def get_radar_chart(input_data):
 
 
 def add_prediction(input_data):
-    model = pickle.load(open("model.pkl", "rb"))
-    scaler = pickle.load(open("scaler.pkl", "rb")) 
+    model = pickle.load(open("model/model.pkl", "rb"))
+    scaler = pickle.load(open("model/scaler.pkl", "rb")) 
 
     input_array = np.array(list(input_data.values())).reshape(1, -1)
     input_array_scaled = scaler.transform(input_array)
